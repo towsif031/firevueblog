2e5c445de591505fa9aadb57066ef2a6c0f6e5e9
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-25 02:04:51 +0600

    base nav styles

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,57 @@
 <template>
-  <div class="app-wrapper">
-    <div class="app">
-      <router-view />
-    </div>
-  </div>
+	<div class="app-wrapper">
+		<div class="app">
+			<Navigation />
+			<router-view />
+		</div>
+	</div>
 </template>
 
 <script>
+import Navigation from './components/Navigation.vue';
+
 export default {
-  name: "app",
-  components: {},
-  data() {
-    return {};
-  },
-  created() {},
-  mounted() {},
-  methods: {},
-  watch: {},
+	name: 'app',
+	components: { Navigation },
+	data() {
+		return {};
+	},
+	created() {},
+	mounted() {},
+	methods: {},
+	watch: {}
 };
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
 
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Quicksand", sans-serif;
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Quicksand', sans-serif;
 }
 
 .app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
 }
 
 .container {
-  max-width: 1440px;
-  margin: 0 auto;
+	max-width: 1440px;
+	margin: 0 auto;
 }
 
 .link {
-  cursor: pointer;
-  text-decoration: none;
-  text-transform: uppercase;
-  color: black;
+	cursor: pointer;
+	text-decoration: none;
+	text-transform: uppercase;
+	color: black;
 }
 
 .link-light {
-  color: #fff;
+	color: #fff;
 }
 </style>
