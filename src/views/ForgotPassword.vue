cea685ebe369898b5ef52fdb45226dd1a59f237b
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-26 02:31:16 +0600

    forgot password page

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,9 +1,79 @@
 <template>
-	<div></div>
+	<div class="reset-password">
+		<Modal
+			v-if="modalActive"
+			:modalMessage="modalMessage"
+			v-on:close-modal="closeModal"
+		/>
+		<Loading v-if="loading" />
+		<div class="form-wrap">
+			<form class="reset">
+				<p class="login-register">
+					Back to
+					<router-link class="router-link" :to="{ name: 'Login' }"
+						>Login</router-link
+					>
+				</p>
+				<h2>Reset Password</h2>
+				<p>Forgot your password? Enter your email to reset it</p>
+				<div class="inputs">
+					<div class="input">
+						<input
+							type="text"
+							placeholder="Email"
+							v-model="email"
+						/>
+						<email class="icon" />
+					</div>
+				</div>
+				<button @click.prevent="resetPassword">Reset</button>
+				<div class="angle"></div>
+			</form>
+			<div class="background"></div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {};
+import email from '../assets/Icons/envelope-regular.svg';
+import Modal from '../components/Modal.vue';
+import Loading from '../components/Loading.vue';
+
+export default {
+	name: 'ForgotPassword',
+	components: { email, Modal, Loading },
+	data() {
+		return {
+			email: null,
+			modalActive: false,
+			modalMessage: '',
+			loading: null
+		};
+	},
+	methods: {
+		closeModal() {
+			this.modalActive = !this.modalActive;
+			this.email = '';
+		}
+	}
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.reset-password {
+	position: relative;
+
+	.form-wrap {
+		.reset {
+			h2 {
+				margin-bottom: 8px;
+			}
+
+			p {
+				text-align: center;
+				margin-bottom: 32px;
+			}
+		}
+	}
+}
+</style>
