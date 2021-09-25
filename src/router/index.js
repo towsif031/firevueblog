6c0b1a1eb66b7f24b92768d610ce821e644a7c21
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-25 23:59:43 +0600

    implemented tab title

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,18 @@ const routes = [
 	{
 		path: '/',
 		name: 'Home',
-		component: Home
+		component: Home,
+		meta: {
+			title: 'Home'
+		}
 	},
 	{
 		path: '/blogs',
 		name: 'Blogs',
-		component: Blogs
+		component: Blogs,
+		meta: {
+			title: 'Blogs'
+		}
 	}
 ];
 
@@ -22,6 +28,11 @@ const router = new VueRouter({
 	mode: 'history',
 	base: process.env.BASE_URL,
 	routes
+});
+
+router.beforeEach((to, from, next) => {
+	document.title = `${to.meta.title} | FireVueBlog`;
+	next();
 });
 
 export default router;
